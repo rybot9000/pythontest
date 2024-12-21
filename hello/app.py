539195a8df23a7f9ca7db2c1211c3d23fbc27a74
rybot9000 <jrgraham@nyu.edu>
2024-12-21 15:51:53 -0800

    again

--- hello/app.py
+++ hello/app.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 
 def index():
-  name = request.args["name"] #creating variable titled "name", setting it to request....
-  #name = request.args.get("name", "world")
-  return render_template("index.html", placeholder=name) #(Creates?) argument (i.e., named parameter) titled "placeholder," value of this argument set to name
+  #name = request.args["name"] #creating variable titled "name", setting it to request....
+  name = request.args.get("name", "world")
+    #.get is a function that comes with a dictionary
+    # if you didn't specify "world" as the fallback, would default to None
+  return render_template("index.html", placeholder=name) 
+#(Creates?) argument (i.e., named parameter) titled "placeholder," value of this argument set to name
 # So recall that Python supports named parameters, which just means that you can pass in multiple arguments to a function. But you can specify them by name. So I am calling one of these arguments placeholder. And I'm setting the value of that argument equal to name, which itself is a variable that I defined just a moment ago. 
 
