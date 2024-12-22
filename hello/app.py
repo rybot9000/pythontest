658950fc283685466ce7bde46ec69581d4b4dcbb
rybot9000 <jrgraham@nyu.edu>
2024-12-21 16:18:05 -0800

    more

--- hello/app.py
+++ hello/app.py
@@ -9,7 +9,17 @@ def index():
   name = request.args.get("name", "world")
     #.get is a function that comes with a dictionary
     # if you didn't specify "world" as the fallback, would default to None
-  return render_template("index.html", placeholder=name) 
+  return render_template("index.html") 
+  #return render_template("index.html", name=name) 
 #(Creates?) argument (i.e., named parameter) titled "placeholder," value of this argument set to name
 # So recall that Python supports named parameters, which just means that you can pass in multiple arguments to a function. But you can specify them by name. So I am calling one of these arguments placeholder. And I'm setting the value of that argument equal to name, which itself is a variable that I defined just a moment ago. 
+#to the left of the equal sign (e.g., placeholder) is the name of the parameter, and to the left
+#of the equal sign is the value of the parameter. Prof. says in flask world often see stuff like
+#name=name, which isn't super bad, maybe helpful to equal self
 
+
+    
+@app.route("/greet")
+def greet():
+  name = request.args.get("name","world")
+  return render_template("greet.html", name=name) 
